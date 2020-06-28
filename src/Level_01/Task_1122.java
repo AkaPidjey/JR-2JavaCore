6208365844aa987649acc9e8b34cf14cbc0d36b2
AkaPidjey <pidjey@gmail.com>
2020-06-28 15:26:30 +0300

    * Title - Помощь шахматной школе # 1122.

---
+++ src/Level_01/Task_1122.java
@@ -0,0 +1,33 @@
+package Level_01;
+
+/**
+ * Title - Помощь шахматной школе # 1122.
+ * @task Добавь общий базовый класс ChessFigure к классам-фигур: (фигуры из шахмат).
+ *
+ * Требования:
+ * •	Добавь в класс Solution публичный класс шахматной фигуры ChessFigure.
+ * •	Класс King должен быть унаследован от класса ChessFigure.
+ * •	Класс Queen должен быть унаследован от класса ChessFigure.
+ * •	Класс Rook должен быть унаследован от класса ChessFigure.
+ * •	Класс Knight должен быть унаследован от класса ChessFigure.
+ * •	Класс Bishop должен быть унаследован от класса ChessFigure.
+ * •	Класс Pawn должен быть унаследован от класса ChessFigure.
+ */
+
+public class Task_1122 {
+    public static void main(String[] args) {}
+
+    public class ChessFigure {}
+
+    public class King extends ChessFigure {}
+
+    public class Queen extends ChessFigure {}
+
+    public class Rook extends ChessFigure {}
+
+    public class Knight extends ChessFigure {}
+
+    public class Bishop extends ChessFigure {}
+
+    public class Pawn extends ChessFigure {}
+}
