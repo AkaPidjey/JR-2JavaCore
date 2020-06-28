f478ebdaf5d07a818c85b2772239ea554691c0ac
AkaPidjey <pidjey@gmail.com>
2020-06-28 14:50:23 +0300

    * Title - Домашние животные # 1102.

---
+++ src/Level_01/Task_1102.java
@@ -0,0 +1,24 @@
+package Level_01;
+
+/**
+ * Title - Домашние животные # 1102.
+ * @task Отредактировать три класса: Pet (домашнее животное), Cat (кот) и Dog (собака).
+ * Унаследовать кота и собаку от животного.
+ *
+ * Требования:
+ * •	В классе Solution должен быть public класс Pet (домашнее животное).
+ * •	В классе Solution должен быть public класс Cat (кот).
+ * •	В классе Solution должен быть public класс Dog (собака).
+ * •	Класс Cat должен быть унаследован от класса Pet.
+ * •	Класс Dog должен быть унаследован от класса Pet.
+ */
+
+public class Task_1102 {
+    public static void main(String[] args) {}
+
+    public class Pet {}
+
+    public class Cat extends Pet {}
+
+    public class Dog extends Pet {}
+}
