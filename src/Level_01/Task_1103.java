49e52f2a5e2981d39aa2ad179b53424284d50316
AkaPidjey <pidjey@gmail.com>
2020-06-28 14:52:17 +0300

    * Title - Межпланетная ферма # 1103.

---
+++ src/Level_01/Task_1103.java
@@ -0,0 +1,36 @@
+package Level_01;
+
+/**
+ * Title - Межпланетная ферма # 1103.
+ * @task Написать шесть классов: Animal (животное), Cow (корова), Pig (свинья), Sheep (овца), Bull (бык) и Goat (козел).
+ * Унаследовать корову, свинью, овцу, быка и козла от животного.
+ *
+ * Требования:
+ * •	В классе Solution должен быть public класс Animal (животное).
+ * •	В классе Solution должен быть public класс Cow (корова).
+ * •	В классе Solution должен быть public класс Pig (свинья).
+ * •	В классе Solution должен быть public класс Sheep (овца).
+ * •	В классе Solution должен быть public класс Bull (бык).
+ * •	В классе Solution должен быть public класс Goat (козел).
+ * •	Класс Cow должен быть унаследован от класса Animal.
+ * •	Класс Pig должен быть унаследован от класса Animal.
+ * •	Класс Sheep должен быть унаследован от класса Animal.
+ * •	Класс Bull должен быть унаследован от класса Animal.
+ * •	Класс Goat должен быть унаследован от класса Animal.
+ */
+
+public class Task_1103 {
+    public static void main(String[] args) {}
+
+    public class Animal {}
+
+    public class Cow extends Animal {}
+
+    public class Pig extends Animal {}
+
+    public class Sheep extends Animal {}
+
+    public class Bull extends Animal {}
+
+    public class Goat extends Animal {}
+}
