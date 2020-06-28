bb8013d335fab17fd2e61232eee0e9da8b37baf5
AkaPidjey <pidjey@gmail.com>
2020-06-28 15:24:49 +0300

    * Title - Чужой код, незнакомый # 1121.

---
+++ src/Level_01/Task_1121.java
@@ -0,0 +1,27 @@
+package Level_01;
+
+/**
+ * Title - Чужой код, незнакомый # 1121.
+ * @task Исправь наследование в классах: (классы Cat, Dog, Pet, House, Airplane).
+ *
+ * Требования:
+ * •	Исправь, если необходимо, наследование в классе Pet.
+ * •	Исправь, если необходимо, наследование в классе Cat.
+ * •	Исправь, если необходимо, наследование в классе Dog.
+ * •	Исправь, если необходимо, наследование в классе House.
+ * •	Исправь, если необходимо, наследование в классе Airplane.
+ */
+
+public class Task_1121 {
+    public static void main(String[] args) {}
+
+    public class Pet {}
+
+    public class Cat extends Pet {}
+
+    public class Dog extends Pet {}
+
+    public class House {}
+
+    public class Airplane {}
+}
