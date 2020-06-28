bc86799cf152917307dad21886d9e993fb9a100c
AkaPidjey <pidjey@gmail.com>
2020-06-28 15:07:03 +0300

    * Title - «Из ребра его» # 1111.

---
+++ src/Level_01/Task_1111.java
@@ -0,0 +1,22 @@
+package Level_01;
+
+/**
+ * Title - «Из ребра его» # 1111.
+ * @task Изменить два класса Adam (Адам) и Eve (Ева).
+ * Унаследовать Еву от Адама.
+ *
+ * Требования:
+ * •	В классе Solution должен быть класс Adam (Адам).
+ * •	Класс Adam должен быть public.
+ * •	В классе Solution должен быть класс Eve (Ева).
+ * •	Класс Eve должен быть public.
+ * •	Класс Eve должен быть унаследован от класса Adam.
+ */
+
+public class Task_1111 {
+    public static void main(String[] args) {}
+    //Адам
+    public class Adam {}
+    //Ева
+    public class Eve extends Adam {}
+}
