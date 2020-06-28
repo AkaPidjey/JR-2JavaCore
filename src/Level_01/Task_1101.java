d778e1a3c92b181fa3a643ac129ab122fe0520d6
AkaPidjey <pidjey@gmail.com>
2020-06-28 14:48:23 +0300

    * Title - Лошадь и Пегас # 1101.

---
+++ src/Level_01/Task_1101.java
@@ -0,0 +1,22 @@
+package Level_01;
+
+/**
+ * Title - Лошадь и Пегас # 1101.
+ * @task Отредактировать два класса: Horse (лошадь) и Pegasus (пегас).
+ * Унаследовать пегаса от лошади.
+ *
+ * Требования:
+ * •	В классе Solution должен быть класс Horse (лошадь).
+ * •	Класс Horse должен быть public.
+ * •	В классе Solution должен быть класс Pegasus (пегас).
+ * •	Класс Pegasus должен быть public.
+ * •	Класс Pegasus должен быть унаследован от класса Horse.
+ */
+
+public class Task_1101 {
+    public static void main(String[] args) {}
+
+    public class Horse {}
+
+    public class Pegasus extends Horse {}
+}
