576e48f882a4a9eeeb11e5f03fdbbf7d6b9b3b98
AkaPidjey <pidjey@gmail.com>
2020-06-28 15:16:44 +0300

    * Title - Цепочка наследования # 1116.

---
+++ src/Level_01/Task_1116.java
@@ -0,0 +1,24 @@
+package Level_01;
+
+/**
+ * Title - Цепочка наследования # 1116.
+ * @task Расставь правильно "цепочку наследования" в классах: Pet (домашнее животное), Cat (кот), Dog (собака).
+ *
+ * Требования:
+ * •	В классе Solution должен быть public класс Pet (домашнее животное).
+ * •	В классе Solution должен быть public класс Cat (кот).
+ * •	В классе Solution должен быть public класс Dog (собака).
+ * •	Правильно унаследуй класс Cat.
+ * •	Правильно унаследуй класс Dog.
+ */
+
+public class Task_1116 {
+
+    public static void main(String[] args) {}
+
+    public class Pet {}
+
+    public class Cat extends Pet {}
+
+    public class Dog extends Pet {}
+}
