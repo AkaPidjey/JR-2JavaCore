591e7904c09c95c93eb150b20aa4c2fc47061c94
AkaPidjey <pidjey@gmail.com>
2020-06-28 15:08:49 +0300

    * Title - Заклятые друзья # 1112.

---
+++ src/Level_01/Task_1112.java
@@ -0,0 +1,22 @@
+package Level_01;
+
+/**
+ * Title - Заклятые друзья # 1112.
+ * @task Изменить два класса AppleIPhone и SamsungGalaxyS2.
+ * Унаследовать SamsungGalaxyS2 от AppleIPhone.
+ *
+ * Требования:
+ * •	В классе Solution должен быть класс AppleIPhone.
+ * •	Класс AppleIPhone должен быть public.
+ * •	В классе Solution должен быть класс SamsungGalaxyS2.
+ * •	Класс SamsungGalaxyS2 должен быть public.
+ * •	Класс SamsungGalaxyS2 должен быть унаследован от класса AppleIPhone.
+ */
+
+public class Task_1112 {
+    public static void main(String[] args) {}
+
+    public class AppleIPhone {}
+
+    public class SamsungGalaxyS2 extends AppleIPhone {}
+}
