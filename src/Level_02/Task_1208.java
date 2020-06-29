9ce1d08bacf174cea09a1514a7236efc8cb35844
AkaPidjey <pidjey@gmail.com>
2020-06-29 23:06:08 +0300

    * Title - Свобода печати # 1208.

---
+++ src/Level_02/Task_1208.java
@@ -0,0 +1,26 @@
+package Level_02;
+
+/**
+ * Title - Свобода печати # 1208.
+ * @task Написать пять методов print с разными параметрами.
+ *
+ * Требования:
+ * •	Программа не должна выводить текст на экран.
+ * •	Класс Solution должен содержать шесть методов.
+ * •	Класс Solution должен содержать статический метод main().
+ * •	Класс Solution должен содержать пять статических методов print() с разными параметрами.
+ */
+
+public class Task_1208 {
+    public static void main(String[] args) {}
+
+    public static void print(int i) {}
+
+    public static void print(Integer i) {}
+
+    public static void print(String s) {}
+
+    public static void print(float f) {}
+
+    public static void print(char c) {}
+}
