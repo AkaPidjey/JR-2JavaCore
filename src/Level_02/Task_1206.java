6c7c237521f46be3cf3faffb22a57ce048d4ed88
AkaPidjey <pidjey@gmail.com>
2020-06-29 23:02:58 +0300

    * Title - Выполняем перегрузку! # 1206.

---
+++ src/Level_02/Task_1206.java
@@ -0,0 +1,21 @@
+package Level_02;
+
+/**
+ * Title - Выполняем перегрузку! # 1206.
+ * @task Написать два метода: print(int) и print(String).
+ *
+ * Требования:
+ * •	Программа не должна выводить текст на экран.
+ * •	Класс Solution должен содержать три метода.
+ * •	Класс Solution должен содержать статический метод main().
+ * •	Класс Solution должен содержать статический метод print() с параметром типа int.
+ * •	Класс Solution должен содержать статический метод print() с параметром типа String.
+ */
+
+public class Task_1206 {
+    public static void main(String[] args) {}
+
+    public static void print(int i) {}
+
+    public static void print(String i) {}
+}
