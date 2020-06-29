3cf334cd1976566b988b1e20a4347c43c5e4ae93
AkaPidjey <pidjey@gmail.com>
2020-06-29 23:19:24 +0300

    * Title - Летать охота # 1216.

---
+++ src/Level_02/Task_1216.java
@@ -0,0 +1,23 @@
+package Level_02;
+
+/**
+ * Title - Летать охота # 1216.
+ * @task Напиши свой public интерфейс CanFly(летать).
+ * Добавь в него два метода.
+ *
+ * Требования:
+ * •	Класс Solution должен содержать интерфейс CanFly.
+ * •	Интерфейс CanFly должен быть публичным.
+ * •	Интерфейс CanFly должен содержать два метода.
+ * •	Интерфейс CanFly не должен иметь родителя.
+ */
+
+public class Task_1216 {
+    public static void main(String[] args) {
+    }
+
+    public interface CanFly {
+        int method1();
+        String method2();
+    }
+}
