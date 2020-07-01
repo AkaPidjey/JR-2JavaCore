d14194d0264763cda51389aa3487235af518eabc
AkaPidjey <pidjey@gmail.com>
2020-07-02 00:19:22 +0300

    * Title - Битва роботов # 1328.

---
+++ src/Level_03/Task_1328/Defensable.java
@@ -0,0 +1,5 @@
+package Level_03.Task_1328;
+
+public interface Defensable {
+    BodyPart defense();
+}
---
+++ src/Level_03/Task_1328/Robot.java
@@ -0,0 +1,22 @@
+package Level_03.Task_1328;
+
+public class Robot extends AbstractRobot{
+    public Robot(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public BodyPart attack() {
+        return super.attack();
+    }
+
+    @Override
+    public BodyPart defense() {
+        return super.defense();
+    }
+}
