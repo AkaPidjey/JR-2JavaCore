9fff9279e8ed88d9ad5653db7edae58d157f3319
AkaPidjey <pidjey@gmail.com>
2020-07-01 23:29:48 +0300

    * Title - Всё, что движется # 1309.

---
+++ src/Level_03/Task_1309.java
@@ -0,0 +1,31 @@
+package Level_03;
+
+/**
+ * Title - Всё, что движется # 1309.
+ * @task 1. Создай интерфейс CanMove с методом speed.
+ * 2. Метод speed должен возвращать значение типа Double и не должен ничего принимать в качестве аргументов.
+ * 3. Создай и унаследуй интерфейс CanFly от интерфейса CanMove.
+ * 4. Добавь в интерфейс CanFly метод speed.
+ * 5. Метод speed должен возвращать значение типа Double и принимать один параметр типа CanFly.
+ *
+ * Требования:
+ * •	В классе Solution должен быть объявлен интерфейс CanMove.
+ * •	В классе Solution должен быть объявлен интерфейс CanFly.
+ * •	Интерфейс CanFly должен наследоваться от интерфейса CanMove.
+ * •	В интерфейсе CanMove должен быть объявлен метод speed без параметров и с типом возвращаемого значения Double.
+ * •	В интерфейсе CanFly должен быть объявлен метод speed c одним аргументом типа CanFly и
+ * с типом возвращаемого значения Double.
+ */
+
+public class Task_1309 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface CanMove {
+        public Double speed();
+    }
+
+    public interface CanFly extends CanMove {
+        public Double speed(CanFly a);
+    }
+}
