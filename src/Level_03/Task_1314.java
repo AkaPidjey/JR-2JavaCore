f8ee694585c297e303947a21fbadfa12cd4da61c
AkaPidjey <pidjey@gmail.com>
2020-07-01 23:42:54 +0300

    * Title - Большая лисица — это такая лисица # 1314.

---
+++ src/Level_03/Task_1314.java
@@ -0,0 +1,38 @@
+package Level_03;
+
+import java.awt.*;
+
+/**
+ * Title - Большая лисица — это такая лисица # 1314.
+ * @task 1. Исправь класс BigFox так, чтобы программа компилировалась.
+ *
+ * Требования:
+ * •	Интерфейс Animal должен быть реализован в классе Fox.
+ * •	В классе Fox должен быть реализован только один метод(getName).
+ * •	В интерфейсе Animal должен быть объявлен метод getColor.
+ * •	Класс BigFox должен быть потомком класса Fox.
+ */
+
+public class Task_1314 {
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+
+    public static class BigFox extends Fox {
+        public  Color getColor() {
+            return Color.GRAY;
+        }
+    }
+}
