5d781f583be42c243bfa8b17e3392f859e95c704
AkaPidjey <pidjey@gmail.com>
2020-07-01 23:51:12 +0300

    * Title - The weather is fine # 1317.

---
+++ src/Level_03/Task_1317/Weather.java
@@ -0,0 +1,5 @@
+package Level_03.Task_1317;
+
+public interface Weather {
+    String getWeatherType();
+}
---
+++ src/Level_03/Task_1317/WeatherType.java
@@ -0,0 +1,7 @@
+package Level_03.Task_1317;
+
+public interface WeatherType {
+    String CLOUDY = "Cloudy";
+    String FOGGY = "Foggy";
+    String FREEZING = "Freezing";
+}
