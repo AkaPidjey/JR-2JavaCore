8dcb43d2dc0dd403e9f5c1fd48bb48d855d2bc55
AkaPidjey <pidjey@gmail.com>
2020-07-03 22:45:46 +0300

    * Title - Дегустация вин # 1410.

---
+++ src/Level_04/Task_1410/Drink.java
@@ -0,0 +1,7 @@
+package Level_04.Task_1410;
+
+public abstract class Drink {
+    public void taste() {
+        System.out.println("Вкусно");
+    }
+}
---
+++ src/Level_04/Task_1410/Wine.java
@@ -0,0 +1,7 @@
+package Level_04.Task_1410;
+
+public class Wine extends Drink {
+    public String getHolidayName() {
+        return "День Рождения";
+    }
+}
